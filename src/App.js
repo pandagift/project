eb617c19e343a19221813c34eb4a2f269a4c61cb
Arapov Vasyl <panda.dagon@gmail.com>
2022-06-06 17:33:23 +0300

    ungovernable component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import Counter from './components/Counter.jsx';
+import PostItem from './components/PostItem.jsx';
+import PostList from './components/PostList.jsx';
+import MyButton from './components/UI/button/MyButton.jsx';
+import MyInput from './components/UI/input/MyInput.jsx';
+import './styles/App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript', body: 'Description' },
+    { id: 3, title: 'Javascript', body: 'Description' },
+  ]);
+  const [title, setTitle] = useState('')
+  const bodyInputRef = useRef();
+  const addNewPost = (e) => {
+    e.preventDefault()
+    console.log(bodyInputRef.current.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type='text'
+          placeholder='Name' />
+        <MyInput
+          ref={bodyInputRef}
+          type='text'
+          placeholder='Description' />
+        <MyButton onClick={addNewPost}>Create post</MyButton>
+      </form>
+      <PostList posts={posts} title="Posts list:" />
     </div>
   );
 }
